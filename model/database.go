d0d138f917848a9e3d69d4e59753f95e5d91a80b
Sinji Prasetio <sinjiprasetio@outlook.com>
2023-03-20 10:57:06 +0700

    Updated Documentation comment and fix and recheck the unconverted ORM

--- model/database.go
+++ model/database.go
@@ -24,7 +24,7 @@ type Model interface {
 	Find(id uint) (interface{}, error)
 	Where(query interface{}, args ...interface{}) *model
 	Save() error
-	Delete(data interface{}) (interface{}, error)
+	Delete() error
 	Load(model interface{}) *model
 	Count() (int64, error)
 	With(relation string) error
@@ -53,37 +53,49 @@ func NewModel(db *gorm.DB) *model {
 	return &model{db, v, db}
 }
 
-// Model specifies the model to be used for subsequent database operations.
+// modelLoad specifies the model to be used for subsequent database operations.
+// It takes in a pointer to the model object and returns a pointer to the model object.
+// This method sets the current model to the specified model object, allowing it to be used for subsequent database operations.
 func (m *model) modelLoad(model interface{}) *model {
 	m.db = m.memoryDB.Model(model)
 	return m
 }
 
 // Load loads data into the specified model object.
+// It takes in a pointer to the model object, loads it into the model, and returns a pointer to the model object.
+// This method loads the specified model object into the current model, allowing it to be used for subsequent database operations.
 func (m *model) Load(model interface{}) *model {
 	m.modelLoad(&model)
 	m.tempData = model
 	return m
 }
 
-// Find searches for a record with the specified ID and returns it.
+// Find searches for a model with the specified ID and returns it.
+// It takes in a uint representing the ID to search for, and returns the found data and an error, if any.
+// If the data is found, the function returns the data with a nil error. If an error occurs, it returns an error object with the corresponding error message.
 func (m *model) Find(id uint) (interface{}, error) {
 	err := m.db.First(m.tempData, id).Error
 	return m.tempData, err
 }
 
 // Where applies the specified query to the model.
+// It takes in a query interface and a variable number of arguments, and returns a pointer to the model object.
+// This method applies the specified query to the current query on the model, with the specified arguments.
 func (m *model) Where(query interface{}, args ...interface{}) *model {
 	m.db = m.db.Where(query, args...)
 	return m
 }
 
 // Get retrieves the records that match the current query.
+// It takes no input parameters and returns an error, if any.
+// If the retrieval is successful, the function returns nil. If an error occurs, it returns an error object with the corresponding error message.
 func (m *model) Get() error {
 	return m.db.Find(m.tempData).Error
 }
 
 // OrderBy specifies the order in which the records should be retrieved.
+// It takes in two strings representing the column and the mode of the order, respectively, and returns a pointer to the model object.
+// This method orders the records in the database by the specified column, with the specified mode of order (ASC or DESC).
 func (m *model) OrderBy(column string, mode string) *model {
 	mode = strings.ToUpper(mode)
 	order := fmt.Sprintf("%s %s", column, mode)
@@ -92,18 +104,24 @@ func (m *model) OrderBy(column string, mode string) *model {
 }
 
 // Limit specifies the maximum number of records to retrieve.
+// It takes in an integer representing the maximum number of records to retrieve, and returns a pointer to the model object.
+// This method limits the number of records that can be retrieved from the database to the specified limit.
 func (m *model) Limit(limit int) *model {
 	m.db = m.db.Limit(limit)
 	return m
 }
 
-// Delete deletes the specified record from the database.
-func (m *model) Delete(data interface{}) (interface{}, error) {
-	err := m.db.Delete(data).Error
-	return data, err
+// Delete : deletes the specified record from the database.
+// It takes no input parameters and returns an error, if any.
+// If the deletion is successful, the function returns nil. If an error occurs, it returns an error object with the corresponding error message.
+func (m *model) Delete() error {
+	err := m.db.Delete(m.tempData).Error
+	return err
 }
 
 // Count returns the total number of records that match the current query.
+// It takes no input parameters and returns the total number of records as an int64 and an error, if any.
+// If the counting is successful, the function returns the total number of records with a nil error. If an error occurs, it returns an error object with the corresponding error message.
 func (m *model) Count() (int64, error) {
 	var count int64
 	err := m.db.Count(&count).Error
@@ -113,7 +131,11 @@ func (m *model) Count() (int64, error) {
 	return count, nil
 }
 
-// Paginate retrieves the records that match the current query and returns pagination information.
+// Get retrieves the records that match the current query.
+// It takes no input parameters and returns an error, if any.
+// If the retrieval is successful, the function returns nil. If an error occurs, it returns an error object with the corresponding error message.
+// It takes in a pointer to the model object, the page number, and the number of records per page, and returns a pointer to a Pagination object and an error, if any.
+// If the pagination is successful, the function returns a pointer to the Pagination object with a nil error. If an error occurs, it returns an error object with the corresponding error message.
 func (m *model) Paginate(model interface{}, page int, perPage int) (*Pagination, error) {
 	var totalRecords int64
 	m.db.Model(model).Count(&totalRecords)
@@ -140,6 +162,8 @@ func (m *model) Paginate(model interface{}, page int, perPage int) (*Pagination,
 }
 
 // Save creates a new record if the model has no ID, or updates an existing record if the model has an ID.
+// It takes no input parameters and returns an error, if any.
+// If the creation or update is successful, the function returns nil. If an error occurs, it returns an error object with the corresponding error message.
 func (m *model) Save() error {
 	err := m.db.Save(m.tempData).Error
 	if err != nil {
